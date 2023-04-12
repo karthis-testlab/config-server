d81261b5849602f6226f9c3254f0f3ec25a4e03a
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-04-12 11:09:04 +0530

    Pushed config-server code into the remote repo

---
+++ src/test/java/com/ta/api/assessment/config/server/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ta.api.assessment.config.server.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
